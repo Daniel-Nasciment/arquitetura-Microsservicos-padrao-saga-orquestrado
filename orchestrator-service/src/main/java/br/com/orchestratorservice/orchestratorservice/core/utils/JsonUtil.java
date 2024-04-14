4c24140fb9ce6777191521d0bc6e776e45fc0a10
Daniel-Nasciment <daniel.fecaf@outlook.com>
2024-04-14 15:20:02 -0300

    Feat: Implementando o JsonUtil

---
+++ orchestrator-service/src/main/java/br/com/orchestratorservice/orchestratorservice/core/utils/JsonUtil.java
@@ -0,0 +1,23 @@
+package br.com.orchestratorservice.orchestratorservice.core.utils;
+
+import br.com.orchestratorservice.orchestratorservice.core.dto.Event;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class JsonUtil {
+
+    private final ObjectMapper objectMapper;
+
+    public String toJson(Object object) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(object);
+    }
+
+    public Event toEvent(String json) throws JsonProcessingException {
+        return objectMapper.readValue(json, Event.class);
+    }
+
+}
