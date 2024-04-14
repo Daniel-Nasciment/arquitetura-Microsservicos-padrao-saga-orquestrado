181305ac7c06c5763e4dee805b1494f6958068ac
Daniel-Nasciment <daniel.fecaf@outlook.com>
2024-04-14 19:15:39 -0300

    Feat: Implementando producers nos micro services

---
+++ payment-service/src/main/java/br/com/service/pament/paymentservice/core/producer/KafkaProducer.java
@@ -0,0 +1,25 @@
+package br.com.service.pament.paymentservice.core.producer;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class KafkaProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Value("${spring.kafka.topic.orchestrator}")
+    private String orchestratorTopic;
+
+    public void sendEvent(String payload){
+        log.info("Sending event to topic {} with data {}", this.orchestratorTopic, payload);
+        kafkaTemplate.send(orchestratorTopic, payload);
+    }
+
+
+}
