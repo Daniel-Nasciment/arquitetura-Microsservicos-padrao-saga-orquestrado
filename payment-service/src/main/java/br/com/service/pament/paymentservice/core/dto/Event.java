d73bb88c63b31a99eb4b8b9e66265910b4cfdc67
Daniel-Nasciment <daniel.fecaf@outlook.com>
2024-05-16 21:22:02 -0300

    Feat: Implementando handle success para pagamento

--- payment-service/src/main/java/br/com/service/pament/paymentservice/core/dto/Event.java
+++ payment-service/src/main/java/br/com/service/pament/paymentservice/core/dto/Event.java
@@ -7,7 +7,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.util.ObjectUtils.isEmpty;
 
 @Data
 @Builder
@@ -24,4 +27,10 @@ public class Event {
     private List<History> eventHistory;
     private LocalDateTime createdAt;
 
+    public void addToHistory(History history){
+        if(isEmpty(this.eventHistory)){
+            this.eventHistory = new ArrayList<>();
+        }
+        eventHistory.add(history);
+    }
 }
--- payment-service/src/main/java/br/com/service/pament/paymentservice/core/service/PaymentService.java
+++ payment-service/src/main/java/br/com/service/pament/paymentservice/core/service/PaymentService.java
@@ -2,7 +2,10 @@ package br.com.service.pament.paymentservice.core.service;
 
 import br.com.service.pament.paymentservice.config.exception.ValidationException;
 import br.com.service.pament.paymentservice.core.dto.Event;
+import br.com.service.pament.paymentservice.core.dto.History;
 import br.com.service.pament.paymentservice.core.dto.OrderProducts;
+import br.com.service.pament.paymentservice.core.enums.EPaymentStatus;
+import br.com.service.pament.paymentservice.core.enums.ESagaStatus;
 import br.com.service.pament.paymentservice.core.model.Payment;
 import br.com.service.pament.paymentservice.core.producer.KafkaProducer;
 import br.com.service.pament.paymentservice.core.repository.PaymentRepository;
@@ -12,6 +15,12 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
+import static br.com.service.pament.paymentservice.core.enums.EPaymentStatus.SUCCESS;
+import static br.com.service.pament.paymentservice.core.enums.ESagaStatus.FAIL;
+import static br.com.service.pament.paymentservice.core.enums.ESagaStatus.ROLLBACK_PENDING;
+
 @Service
 @AllArgsConstructor
 @Slf4j
@@ -19,6 +28,7 @@ public class PaymentService {
 
     private static final String CURRENT_SOURCE = "PAYMENT_SERVICE";
     public static final Double REDUCE_SUM_VALUE = 0.0;
+    public static final Double MIN_AMOUNT_VALUE = 0.1;
     private final KafkaProducer kafkaProducer;
     private final JsonUtil jsonUtil;
     private final PaymentRepository paymentRepository;
@@ -28,12 +38,18 @@ public class PaymentService {
         try {
             checkCurrentValidation(event);
             createPendingPayment(event);
+            Payment payment = findByOrderIdAndTransactionId(event);
+            validateAmount(payment.getTotalAmount());
+            changePaymentToSuccess(payment);
+            handleSuccess(event);
         }catch (Exception ex) {
             log.error("Error trying to make payment: ", ex);
         }
         kafkaProducer.sendEvent(jsonUtil.toJson(event));
 
     }
+
+
 
     private void checkCurrentValidation(Event event) {
 
@@ -46,7 +62,7 @@ public class PaymentService {
 
     private void createPendingPayment(Event event) {
 
-        var totalAmount = calculateAmout(event);
+        var totalAmount = calculateAmount(event);
         var totalItems = calculateTotalItems(event);
 
         Payment payment = Payment.builder()
@@ -56,11 +72,11 @@ public class PaymentService {
                 .totalItems(totalItems)
                 .build();
 
-        setEventAmoutItems(event, payment);
+        setEventAmountItems(event, payment);
 
     }
 
-    private double calculateAmout(Event event){
+    private double calculateAmount(Event event){
         return event
                 .getPayload()
                 .getProducts()
@@ -78,13 +94,62 @@ public class PaymentService {
                 .reduce(REDUCE_SUM_VALUE.intValue(), Integer::sum);
     }
 
-    private void setEventAmoutItems(Event event, Payment payment){
+    private void setEventAmountItems(Event event, Payment payment){
         event.getPayload().setTotalAmount(payment.getTotalAmount());
         event.getPayload().setTotalItems(payment.getTotalItems());
+    }
+
+
+    private void validateAmount(double amount){
+        if(amount < MIN_AMOUNT_VALUE) {
+            throw new ValidationException("The minimum amount available is ".concat(MIN_AMOUNT_VALUE.toString()));
+        }
+    }
+
+    private Payment findByOrderIdAndTransactionId(Event event){
+        return paymentRepository.findByOrderIdAndTransactionId(event.getPayload().getId(), event.getTransactionId()).orElseThrow(() -> new ValidationException("Payment not found by OrderId and TransactionId"));
+    }
+
+    private void changePaymentToSuccess(Payment payment) {
+        payment.setStatus(SUCCESS);
+        save(payment);
     }
 
     private void save(Payment payment){
         paymentRepository.save(payment);
     }
+
+    private void handleSuccess(Event event) {
+        event.setStatus(ESagaStatus.SUCCESS);
+        event.setSource(CURRENT_SOURCE);
+        addHistory(event, "Payment realized successfully!");
+    }
+
+    private void addHistory(Event event, String message){
+        History history = History.builder()
+                .source(event.getSource())
+                .status(event.getStatus())
+                .message(message)
+                .createdBy(LocalDateTime.now())
+                .build();
+        event.addToHistory(history);
+    }
+
+
+    private void handleFailCurrentNotExecuted(Event event, String message) {
+        event.setStatus(ROLLBACK_PENDING);
+        event.setSource(CURRENT_SOURCE);
+        addHistory(event, "Fail to make payment: ".concat(message));
+    }
+
+
+    public void rollbackEvent(Event event) throws JsonProcessingException {
+        //changeValidationToFail(event);
+        event.setStatus(FAIL);
+        event.setSource(CURRENT_SOURCE);
+        addHistory(event, "Rollback executed on product validation!");
+        kafkaProducer.sendEvent(jsonUtil.toJson(event));
+    }
+
 
 }
